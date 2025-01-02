3a2c98d3b76fda8ee49d5a5b02779f55255c0c09
Boopathi <boopathibm19@gmail.com>
2025-01-02 14:24:21 +0530

    Array demo

--- app/src/main/java/demo/App.java
+++ app/src/main/java/demo/App.java
@@ -8,7 +8,15 @@ public class App {
         return "Hello World!";
     }
 
+    public String getGreeting1(){
+        return "welcome to gradle";
+    }
+
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        System.out.println( new App().getGreeting1());
+
+        DemoString eg1 = new DemoString();
+        eg1.demoString();
     }
 }
---
+++ app/src/main/java/demo/DemoString.java
@@ -0,0 +1,15 @@
+package demo;
+
+class DemoString{
+    public void demoString()
+    {
+        String someString = "this is a lot of text again";
+        someString.length();
+        System.out.println(someString.charAt(5));
+        for(int i=0; i<someString.length();i++){
+            System.out.println(someString.charAt(i));
+                       
+            }
+
+    }
+}
